69bca60d0a3f0e537cc96cd0d0c949212d408a65
“kaungthant-w” <kaungthant9513@gmail.com>
2022-09-20 23:35:36 +0630

    Update Feature

--- index.php
+++ index.php
@@ -19,19 +19,19 @@ require("config.php");
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
 <body class="app">
-    <div class="container mt-5 p-3 bg-light h-full rounded">
+    <div class="container mt-0 mt-sm-5 p-3 bg-light h-full rounded">
         <div class="p-3 mt-2 h2">
             <a href="#" data-bs-toggle="modal" data-bs-target="#add"><i class="fa-solid fa-plus"></i></a>
             Notes
         </div>
         <div class="my-5 ">
-          <div class="row ml-3">
+            <div class="row ml-3">
               <?php
                   if($result) {
                     foreach($result as $value) {
                       ?>
-                      <div class="col mb-3">
-                          <div class="card bg-secondary" style="width: 400px;height:200px;">
+                      <div class="col-12 col-md-6 col-lg-4 mb-3 noteBody">
+                          <div class="card bg-secondary" style="height:200px;">
                               <div class="card-body text-white">
                                   <h4 class="card-title"> <?php echo $value["title"]; ?>
                                   </h4>
@@ -39,11 +39,13 @@ require("config.php");
 
                                   <p class="card-text"><?php echo $value["description"]; ?></p>
                                   
-                                  <a href="delete.php?id=<?php echo $value["id"]; ?>" class="card-link text-danger h4"><span><i class="fa-solid fa-trash"></i></span>
-                                  </a>
+                                  <div class="mt-3">
+                                    <a href="delete.php?id=<?php echo $value["id"]; ?>" class="card-link text-danger h4"><span><i class="fa-solid fa-trash"></i></span>
+                                    </a>
 
-                                  <a href="#" class="card-link text-warning float-end h4"><span><i class="fa-solid fa-pen-to-square" data-bs-toggle="modal" data-bs-target="#edit1<?php echo $value['id']?>"></i></span>
-                                  </a>
+                                    <a href="edit.php?id=<?php echo $value["id"]; ?>" class="card-link text-warning float-end h4" ><span><i class="fa-solid fa-pen-to-square" data-bs-toggle="modal" data-bs-target="#edit1"></i></span>
+                                    </a>
+                                  </div>
 
                               </div>
                             </div>
@@ -56,7 +58,9 @@ require("config.php");
         </div>
     </div>
 
+<script src="js/jquery.js"></script>
 <script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/note.js"></script>
 </body>
 </html>
 
@@ -72,12 +76,12 @@ require("config.php");
       <div class="modal-body">
        <form action="add.php" method="POST">
             <div class="form-group">
-                <label for="title" class="heading">Title</label>
+                <label for="title">Title</label>
                 <input type="text" name="title" class="form-control">
             </div>
 
             <div class="form-group">
-                <label for="description" class="heading">Description</label>
+                <label for="description">Description</label>
                 <textarea class="form-control" name="description"  cols="30" rows="10"></textarea>
             </div>
 
@@ -88,38 +92,6 @@ require("config.php");
        </form>
       </div>
       
-    </div>
-  </div>
-</div>
-
-<!-- edit  -->
-<div class="modal fade" id="edit1<?php echo $value['id']?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Update Note</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-      <form action="edit.php" method="post">
-        <input type="hidden" name="id" value="<?php $result[0]['id']; ?>">
-            <div class="form-group">
-                <label for="title" class="heading">Title</label>
-                <input type="text" name="title" class="form-control" value="<?php echo $result[0]["title"]; ?>">
-            </div>
-
-            <div class="form-group">
-                <label for="desc" class="heading">Description</label>
-                <textarea class="form-control" name="desc" id="" cols="30" rows="10">
-                  <?php echo $result[0]["description"]; ?>
-                </textarea>
-            </div>
-       </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Update</button>
-      </div>
     </div>
   </div>
 </div>
